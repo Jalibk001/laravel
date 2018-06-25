31049d51579d1a930dd445ca5eb2411df1f710d6
Jalibk001 <14bscsjkhan@seecs.edu.pk>
2018-06-25 07:10:38 +0500

    abc

--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -14,7 +14,11 @@
 	</title>
 	<script type="text/javascript">
 	$(document).ready(function()     
-  	{  		
+  	{  
+  		$('#div_id').click(function(){
+       	a=document.getElementById("dt");
+       	console.log(a);
+       })		
   	   $.ajax({
        url: '/Get_Tweet',
        type: 'get',
@@ -42,90 +46,16 @@
        {
          alert("error");
        }});
-  	})
-		
+
+  	})		
 	</script>
-	<style type="text/css">
-		.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-.topnav .search-container {
-  float: right;
-}
-
-.topnav input[type=text] {
-  padding: 6px;
-  margin-top: 8px;
-  font-size: 17px;
-  border: none;
-}
-
-.topnav .search-container button {
-  float: right;
-  padding: 6px;
-  margin-top: 8px;
-  margin-right: 16px;
-  background: #ddd;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-.topnav .search-container button:hover {
-  background: #ccc;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav .search-container {
-    float: none;
-  }
-  .topnav a, .topnav input[type=text], .topnav .search-container button {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-}
-</style>
-
 </head>
 <body>
 	<center><h1>Tweets</h1></center>
-<div class="topnav">
-  <div class="search-container">
-    <form >
-      <input type="text" placeholder="Search.." name="search">
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-</div>
+	<form id="dt">
+		<input type="text" name="search"/>
+		<button id="sr">Search</button>
+		</form>
 <p id="div_id"></p>
 
 </body>
